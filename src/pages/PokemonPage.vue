5bb02b1637aec81376aade2a5c23d80e738eff6f
thusspokedata <antonio.datahack@gmail.com>
2022-12-13 00:10:40 +0100

    get 4 names (#93)

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,36 @@
+<template>
+    <h1>Quien es este pokemon?</h1>
+    <PokemonPicture :pokemonId="4" :showPokemon="true" />
+    <PokemonOptions :pokemons="pokemonArr"/>
+</template>
+
+<script>
+
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+console.log(getPokemonOptions())
+
+export default {
+  name: 'PokemonPage',
+  components: {
+    PokemonPicture,
+    PokemonOptions
+  }, 
+  data() {
+    return {
+      pokemonArr: []
+    }
+  },
+  methods: {
+    async mixPokemonArr() {
+      this.pokemonArr = await getPokemonOptions()
+      console.log(this.pokemonArr)
+    }
+  }, 
+  mounted() {
+    this.mixPokemonArr()
+  }
+}
+</script>
